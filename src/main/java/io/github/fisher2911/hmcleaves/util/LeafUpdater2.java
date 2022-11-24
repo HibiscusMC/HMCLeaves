cd6b245ab0888be2a8929b29c5c33f9937931cef
Fisher2911 <fisher1205l@gmail.com>
2022-11-24 13:09:07 -0500

    Added back copyright

--- src/main/java/io/github/fisher2911/hmcleaves/util/LeafUpdater2.java
+++ src/main/java/io/github/fisher2911/hmcleaves/util/LeafUpdater2.java
@@ -20,26 +20,6 @@
 
 package io.github.fisher2911.hmcleaves.util;
 
-/*
- *
- *  *     HMCLeaves
- *  *     Copyright (C) 2022  Hibiscus Creative Studios
- *  *
- *  *     This program is free software: you can redistribute it and/or modify
- *  *     it under the terms of the GNU General Public License as published by
- *  *     the Free Software Foundation, either version 3 of the License, or
- *  *     (at your option) any later version.
- *  *
- *  *     This program is distributed in the hope that it will be useful,
- *  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  *     GNU General Public License for more details.
- *  *
- *  *     You should have received a copy of the GNU General Public License
- *  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- */
-
 public class LeafUpdater2 {
 
 //    private static final HMCLeaves PLUGIN = HMCLeaves.getPlugin(HMCLeaves.class);
