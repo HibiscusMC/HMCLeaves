542863379530898e4c771f97dc154c18564f7897
Fisher2911 <fisher1205l@gmail.com>
2025-06-28 00:44:23 -0400

    Fix block break speed and go back to SQLite database

---
+++ nms/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+}
+
+group = "com.hibiscuscmc.hmcleaves.nms"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+tasks {
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(21)
+        options.compilerArgs.add("-parameters")
+    }
+}
